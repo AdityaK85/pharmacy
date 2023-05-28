123d9ca3aaea2a920bb4e084571c94fbf634385e
Aditya85 <adityakothekar79@gmail.com>
2023-05-28 23:39:29 +0530

    today

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -7,4 +7,12 @@ from .models import *
 
 @admin.register(CustomerMaster)
 class CustomerMasterAdmin(admin.ModelAdmin):
-    list_display=['id','customer_name','customer_mobile', 'payment_mode', 'payment_bal']
+    list_display=['id','customer_name','customer_mobile', 'payment_mode', 'payment_bal', 'created_dt']
+
+@admin.register(MedicineMaster)
+class MedicineMasterAdmin(admin.ModelAdmin):
+    list_display=['id','medicine_name','batch_no', 'medicine_unit', 'medicine_exp', 'medicine_qty', 'medicine_price', 'created_dt']
+
+@admin.register(BuyMaster)
+class BuyMasterAdmin(admin.ModelAdmin):
+    list_display=['id', 'fk_customer', 'fk_medicine','discount','tax', 'paid_amt', 'net_total', 'payment_status', 'created_dt']
--- pharmacy/models.py
+++ pharmacy/models.py
@@ -9,9 +9,36 @@ PAYMENT_CHOICES = (
 		("ONLINE PAY", "ONLINE PAY"),
 	)
 
+PAYMENT_STATUS = (
+		("FULL PAID", "FULL PAID"),
+		("BALANCE", "BALANCE"),
+	)
+
 class CustomerMaster(models.Model):
     customer_name = models.CharField(max_length=100)
     customer_email = models.CharField(max_length=100)
     customer_mobile = models.CharField(max_length=100)
     payment_mode = models.CharField(verbose_name=" Payments Mode", max_length=100 , choices=PAYMENT_CHOICES)
     payment_bal = models.CharField(verbose_name=" Payments Balance", max_length=100)
+    payment_date = models.DateField(null=True)
+    created_dt = models.DateTimeField(null=True)
+
+
+class MedicineMaster(models.Model):
+    medicine_name = models.CharField(max_length=100)
+    batch_no = models.CharField(max_length=100)
+    medicine_unit = models.CharField(max_length=100)
+    medicine_exp = models.DateField(null=True)
+    medicine_qty = models.CharField(max_length=100)
+    medicine_price = models.CharField(max_length=100)
+    created_dt = models.DateTimeField(null=True)
+
+class BuyMaster(models.Model):
+    fk_customer = models.ForeignKey(CustomerMaster, on_delete=models.CASCADE,blank=  True , null= True)
+    fk_medicine = models.ForeignKey(MedicineMaster, on_delete=models.CASCADE,blank=  True , null= True)
+    discount = models.CharField(max_length=100)
+    tax = models.CharField(max_length=100)
+    paid_amt = models.CharField(max_length=100)
+    net_total = models.CharField(max_length=100)
+    payment_status = models.CharField(verbose_name=" Payments Status", max_length=100 , choices=PAYMENT_STATUS)
+    created_dt = models.DateTimeField(null=True)
