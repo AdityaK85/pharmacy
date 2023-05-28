fcdeed6e47cca8e8932f9e3dc5ddc70b1ba52db1
Aditya85 <adityakothekar79@gmail.com>
2023-05-28 17:54:33 +0530

    today

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# Register your models here.
+
+@admin.register(CustomerMaster)
+class CustomerMasterAdmin(admin.ModelAdmin):
+    list_display=['id','customer_name','customer_mobile', 'payment_mode', 'payment_bal']
--- pharmacy/models.py
+++ pharmacy/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# user : pharmacy
+# pass : 123
 
 # Create your models here.
+
+PAYMENT_CHOICES = (
+		("CASH", "CASH"),
+		("ONLINE PAY", "ONLINE PAY"),
+	)
+
+class CustomerMaster(models.Model):
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.CharField(max_length=100)
+    customer_mobile = models.CharField(max_length=100)
+    payment_mode = models.CharField(verbose_name=" Payments Mode", max_length=100 , choices=PAYMENT_CHOICES)
+    payment_bal = models.CharField(verbose_name=" Payments Balance", max_length=100)
